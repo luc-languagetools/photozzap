b012e1dbe20ee500d7f21921afddd51ae6ac5044
Luc Wastiaux <luc.wastiaux@airpost.net>
2013-07-16 23:37:23 +0800

    added example to checkout tag

--- photozzap/server_setup/create_server.py
+++ photozzap/server_setup/create_server.py
@@ -14,6 +14,7 @@ if len(argv) != 4:
 # param 2: image, can be "photozzap-master-0716"
 # param 3: git command line, can be "git pull origin load-balancing-1; git checkout load-balancing-1"
 #                                or "git pull origin master; git checkout master"
+#                                or "git fetch; git checkout tags/v0.5" for a tag
    
     
 
