c50dce18045b768aa60fd0fa46521b106da2a4c7
luc wastiaux <luc.wastiaux@airpost.net>
2014-04-20 10:55:42 +0000

    generated assets for test-0420-7

--- photozzap/scripts/deploy.py
+++ photozzap/scripts/deploy.py
@@ -1,14 +1,25 @@
 import sys
 import photozzap.scripts.deploy_static_assets
+import subprocess
 
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
     )
 
-def git_commit():
+def git_commit(server_name):
     # git add photozzap/static/nodejs/*
     # git add photozzap/static/cdn-files/*
+    # git commit -a -m 'initial import of cdn-files and nodejs'
+    # git tag -a v1.4 -m 'my version 1.4'
+    # git push origin --tags
+    
+    subprocess.check_call("git add photozzap/static/nodejs/*", shell=True)
+    subprocess.check_call("git add photozzap/static/nodejs/*", shell=True)
+    commit_cmd_line = "git commit -a -m 'generated assets for " + server_name + "'"
+    subprocess.check_call(commit_cmd_line, shell=True)
+    tag_cmd_line = "git tag -a v" + server_name + " -m 'deployed version " + server_name + "'"
+    subprocess.check_call(tag_cmd_line, shell=True)
     
     
 if __name__ == "__main__":
@@ -22,3 +33,4 @@ if __name__ == "__main__":
     settings['server_name'] = server_name
     
     photozzap.scripts.deploy_static_assets.manage_assets(settings)
+    git_commit(server_name)
---
+++ photozzap/static/nodejs/config-test-0420-6.js
@@ -0,0 +1,10 @@
+var config = {
+    firebaseRoot: "https://fiery-fire-5557.firebaseio.com/",
+    serverName: "test-0420-6",
+    firebaseSecret: "EcO3bQl1ZvURjixUXhjdDqc0YsJBvh46h1DJxKsq",
+    cloudinaryName: 'photozzap', 
+    cloudinaryApiKey: '751779366151643', 
+    cloudinaryApiSecret: 'OxGxJe0f-jGo1BAsmmlDdRPb7NM'     
+};
+
+module.exports = config;
