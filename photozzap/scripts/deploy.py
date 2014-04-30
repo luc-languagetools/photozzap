ade4ba65ce3be1b8d29ba3e57a2cbf76351c9ba6
luc wastiaux <luc.wastiaux@airpost.net>
2014-04-30 14:24:14 +0000

    automatic deployment on target server

--- photozzap/scripts/deploy.py
+++ photozzap/scripts/deploy.py
@@ -7,7 +7,7 @@ from pyramid.paster import (
     setup_logging,
     )
 
-def git_commit(server_name):
+def git_commit(server_name, deploy_server_name):
     # git add photozzap/static/nodejs/*
     # git add photozzap/static/cdn-files/*
     # git commit -a -m 'initial import of cdn-files and nodejs'
@@ -20,6 +20,19 @@ def git_commit(server_name):
     subprocess.check_call(commit_cmd_line, shell=True)
     tag_cmd_line = "git tag -a v" + server_name + " -m 'deployed version " + server_name + "'"
     subprocess.check_call(tag_cmd_line, shell=True)
+    
+    git_push_cmd_line = "git push"
+    subprocess.check_call(git_push_cmd_line, shell=True)
+    
+    git_pull_on_deploy_server_cmd_line = 'ssh prod@' + deploy_server_name + ' "cd photozzap; git pull"'
+    subprocess.check_call(git_pull_on_deploy_server_cmd_line, shell=True)
+    
+    setup_upstart_entry_cmd_line = 'ssh root@' + deploy_server_name + ' "touch /etc/node/' + server_name + '.conf"'
+    subprocess.check_call(setup_upstart_entry_cmd_line, shell=True)
+    
+    start_node_cmd_line = 'ssh root@' + deploy_server_name + ' "start node NAME=' + server_name + '"'
+    subprocess.check_call(start_node_cmd_line, shell=True)
+    
 
 def upload_to_cdn(server_name):
     upload_cmd_line = "python2.7 configure_cdn.py " + server_name
@@ -34,7 +47,8 @@ if __name__ == "__main__":
     server_name = argv[2]
     settings = get_appsettings(config_uri)
     settings['server_name'] = server_name
+    deploy_server_name = settings['deploy_server_name']
     
     photozzap.scripts.deploy_static_assets.manage_assets(settings)
-    git_commit(server_name)
+    git_commit(server_name, deploy_server_name)
     upload_to_cdn(server_name)
