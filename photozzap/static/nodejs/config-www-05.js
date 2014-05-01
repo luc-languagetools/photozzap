b4b4ab76b34919eca7dd61971dfe18cc86caf677
luc wastiaux <luc.wastiaux@airpost.net>
2014-05-01 06:06:37 +0000

    generated assets for www-05

---
+++ photozzap/static/nodejs/config-www-05.js
@@ -0,0 +1,12 @@
+var config = {
+    firebaseRoot: "https://photozzap-prod.firebaseio.com/",
+    serverName: "www-05",
+    firebaseSecret: "IclKaZ4YluKtpIBT08hgpoRHSFtUGOLlYbEhV7uJ",
+    cloudinaryName: 'photozzap', 
+    cloudinaryApiKey: '751779366151643', 
+    cloudinaryApiSecret: 'OxGxJe0f-jGo1BAsmmlDdRPb7NM',
+    pushoverToken: 'a75ZiiZaRDuCs7mbfPJdmbuD11vS6x',
+    pushoverUser: 'us6it2T5Ecf6gS3DAiogJqU5rzNHjS', 
+};
+
+module.exports = config;
