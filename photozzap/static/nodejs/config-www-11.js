3e487cacdb089a402fa944ab319208464fee748d
luc wastiaux <luc.wastiaux@airpost.net>
2014-08-03 11:29:56 +0000

    generated assets for www-11

---
+++ photozzap/static/nodejs/config-www-11.js
@@ -0,0 +1,13 @@
+var config = {
+    firebaseRoot: "https://photozzap-prod.firebaseio.com/",
+    serverName: "www-11",
+    firebaseSecret: "IclKaZ4YluKtpIBT08hgpoRHSFtUGOLlYbEhV7uJ",
+    cloudinaryName: 'photozzap', 
+    cloudinaryApiKey: '751779366151643', 
+    cloudinaryApiSecret: 'OxGxJe0f-jGo1BAsmmlDdRPb7NM',
+    pushoverToken: 'a75ZiiZaRDuCs7mbfPJdmbuD11vS6x',
+    pushoverUser: 'us6it2T5Ecf6gS3DAiogJqU5rzNHjS', 
+    env: 'prod'
+};
+
+module.exports = config;
