7118ef5108fa8777f6d320d9ea3a344d2f4cbe85
luc wastiaux <luc.wastiaux@airpost.net>
2014-08-14 08:47:29 +0000

    delete files in bulk at the end

--- photozzap/nodejs/cloudinary-list-files.js
+++ photozzap/nodejs/cloudinary-list-files.js
@@ -9,7 +9,7 @@ cloudinary.config({
 
 var current_timestamp =  new Date().getTime();
 //        if (current_timestamp - comment_data.time_added < 120000 &&
-var min_diff = 24*60*60 * 30 * 3;
+var min_diff = 24*60*60 * 14; // more than 7 days old
 
 var image_ids_to_delete = [];
 
@@ -20,8 +20,11 @@ cloudinary.api.resources(function(result)  {
 		var entry = results[i];
 		var created_at_date = new Date(entry.created_at);
 		var time_diff = current_timestamp - created_at_date;
-		if (time_diff > min_diff && entry.tags.length == 0) {
-			console.log("public_id: ", entry.public_id, " created_at: ", entry.created_at, " tags: ", entry.tags);
+		if (time_diff > min_diff && 
+		    (entry.tags.indexOf("prod") > -1 ||
+		     entry.tags.indexOf("test") > -1 ||
+		     entry.tags.indexOf("dev") > -1)) { // prod, test or dev images more than a week old
+			console.log("scheduled for deletion: public_id: ", entry.public_id, " created_at: ", entry.created_at, " tags: ", entry.tags);
 			
 			image_ids_to_delete.push(entry.public_id);
 			
@@ -30,15 +33,22 @@ cloudinary.api.resources(function(result)  {
 			" time_diff: ", time_diff, " min_diff: ", min_diff, " entry: ", entry);
 		}
 		//console.log(entry);
-	}
-}, {tags: true, direction: "asc", max_results: 500});
-
-
-var delete_old_images = false;
-if (delete_old_images) {
-	cloudinary.api.delete_resources(image_ids_to_delete, function(result){
-		console.log(result);
-	});
-}
+		
+	}	
+		
+	var delete_old_images = false;
+	if (delete_old_images) {
+		console.log("public_ids scheduled for deletion: ", image_ids_to_delete);
 			
+		cloudinary.api.delete_resources(image_ids_to_delete, function(result){
+			console.log(result);
+		});
+	} else {
+		console.log("not deleting, image ids: ", image_ids_to_delete);
+	}		
+
+	
+}, {tags: true, direction: "asc", max_results: 50});
+
+
 
