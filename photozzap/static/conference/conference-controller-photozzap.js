85596b5413419f160516ccf2f76652629ab7aa9b
luc wastiaux <luc.wastiaux@airpost.net>
2014-09-06 11:28:09 +0000

    add button to download currently viewed photo

--- photozzap/static/conference/conference-controller-photozzap.js
+++ photozzap/static/conference/conference-controller-photozzap.js
@@ -580,6 +580,14 @@ function PhotozzapCtrl($scope, $rootScope, $firebase, $firebaseSimpleLogin, $mod
 
     }
     
+    $scope.download_current_photo = function() {
+        // $scope.conference_user_object.viewing_image_id
+        var photo_url = $scope.cloudinary_photo_download_url({id: $scope.conference_user_object.viewing_image_id});
+        var ifrm = document.getElementById('download_frame');
+        ifrm.src = photo_url;
+    };
+    
+    
     $scope.cloudinary_photo_full_url = function(image_data) {
         return $.cloudinary.url(image_data.id + ".jpg", {crop: 'fit', 
                                                          width: $scope.full_params.width, 
@@ -592,6 +600,10 @@ function PhotozzapCtrl($scope, $rootScope, $firebase, $firebaseSimpleLogin, $mod
                                                          width: $scope.default_params.width, 
                                                          height: $scope.default_params.height,
                                                          quality: $scope.default_params.quality});
+    };
+    
+    $scope.cloudinary_photo_download_url = function(image_data) {
+        return $.cloudinary.url(image_data.id + ".jpg", {flags: 'attachment'});
     };
     
     $scope.cloudinary_thumbnail_url = function(image_id) {
