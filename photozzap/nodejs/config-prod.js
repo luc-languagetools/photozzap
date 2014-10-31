082d9f2e48b5285edb181ea605e1db0c8615ab40
luc wastiaux <luc.wastiaux@airpost.net>
2014-10-31 14:16:07 +0000

    added buffer profile id in config

--- photozzap/nodejs/config-prod.js
+++ photozzap/nodejs/config-prod.js
@@ -1,6 +1,8 @@
 var config = {
     firebaseRoot: "https://photozzap-prod.firebaseio.com",
     firebaseSecret: "IclKaZ4YluKtpIBT08hgpoRHSFtUGOLlYbEhV7uJ",
+    bufferAccessToken: "1/34698604507dde4810e26d0e29c93405",
+    bufferProfileId: "54119ce618070b071eb3ae64",
 };
 
 module.exports = config;
--- photozzap/nodejs/post_to_buffer.js
+++ photozzap/nodejs/post_to_buffer.js
@@ -12,46 +12,19 @@ cloudinary.config({
   api_secret: config.cloudinaryApiSecret
 });
 
-/*
-BufferApp.prototype.getUserFromToken = function(accessToken) {
-	var oauth2 = this._oauth2Options;
-	return new BufferUser(accessToken, oauth2.api_base);
-};
-*/
-
-// obtain access token from https://bufferapp.com/developers/apps
-var bufferapp = new BufferApp({
-    clientID : "544c6557c64066d84e38d761",
-    clientSecret : "03e1ad5d6e693729c54d6fae04784539",
-    callbackURL : "urn:ietf:wg:oauth:2.0:oob"
-});
-
-var bufferAccessToken = "1/34698604507dde4810e26d0e29c93405";
-
-
-// var user = new BufferUser(access_token, oauth2.api_base);
-// var user = bufferapp.getUserFromToken(bufferAccessToken);
-// var user = BufferApp.BufferUser(bufferAccessToken, bufferapp.api_base);
+var bufferAccessToken = config.bufferAccessToken;
 var user = new BufferUser(bufferAccessToken, 'https://api.bufferapp.com/1');
 
-console.log("user: ", user);
-
 /*
+// get all profiles
 user.getAllProfiles(function(error, profiles){
     console.log("profiles: ", profiles);
 });
 */
 
-var photozzapId = '54119ce618070b071eb3ae64';
 
-user.createStatus('test update', [photozzapId], false, false, false, {}, null, 
+user.createStatus('test update 2', [config.bufferProfileId], false, false, false, {}, null, 
 function(error, new_status) {
     console.log("posted status ", new_status);
 });
 
-/*
-user.getProfile('54119ce618070b071eb3ae64', // this is the photozzap profile
-function(error, profile) {
-    console.log("profile: ", profile);
-});
-*/
