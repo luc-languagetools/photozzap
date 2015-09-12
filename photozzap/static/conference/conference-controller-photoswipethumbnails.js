a54b097da1483cb6fc03876210022aac55d44a07
luc wastiaux <luc.wastiaux@airpost.net>
2015-09-12 03:55:17 +0000

    working minimalistic photoswipe directive

--- photozzap/static/conference/conference-controller-photoswipethumbnails.js
+++ photozzap/static/conference/conference-controller-photoswipethumbnails.js
@@ -7,6 +7,48 @@ function PhotoswipeThumbnailsCtrl($scope, $log) {
 
     $scope.thumbnail_click = function(index) {
         $log.info("photoswipeThumbnailsCtrl.thumbnail_click");
+        
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+        
+        options = {
+            index: index,
+            history: false,
+            showHideOpacity:true,
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of docs for more info
+                var thumbnail = document.getElementById('thumbnail_id_' + index);
+                var pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect();
+
+                return { x:rect.left, y:rect.top + pageYScroll, w:rect.width };
+            },
+            maxSpreadZoom: 4,
+            getDoubleTapZoom: function(isMouseClick, item) {
+                // isMouseClick          - true if mouse, false if double-tap
+                // item                  - slide object that is zoomed, usually current
+                // item.initialZoomLevel - initial scale ratio of image
+                //                         e.g. if viewport is 700px and image is 1400px,
+                //                              initialZoomLevel will be 0.5
+                if(isMouseClick) {
+                    // is mouse click on image or zoom icon
+                    // zoom to original
+                    return 1;
+                    // e.g. for 1400px image:
+                    // 0.5 - zooms to 700px
+                    // 2   - zooms to 2800px
+                } else {
+                    // is double-tap
+                    // zoom to original if initial zoom is less than 0.7x,
+                    // otherwise to 1.5x, to make sure that double-tap gesture always zooms image
+                    return item.initialZoomLevel < 0.7 ? 1 : 1.5;
+                }
+            }
+        };
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, $scope.photoswipe_images, options);
+        gallery.init();        
+        
     };
     
 };
