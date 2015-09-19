eb323cb07d35bc4ae665f33517a09c5ad30dc24b
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-09-19 23:15:31 +0800

    return stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var debug = require('gulp-debug');
 
-var js_path = 'photozzap/static/**/*.js';
+// var js_path = './photozzap/static/*/**.js';
+var js_path = './photozzap/static/**/*.js';
 
 gulp.task('default', function() {
-  gulp.src(js_path)
+  return gulp.src(js_path)
   .pipe(debug());
 });
 
 
-
 gulp.task('scripts', function() {
-  gulp.src(js_path)
+  return gulp.src(js_path)
+  .pipe(concat('all.min.js'))
   .pipe(uglify())
   .pipe(gulp.dest('dist'));
 });
