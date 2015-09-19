5610c76bda3ace6396ac139b8258714f2f153c58
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-09-19 22:38:24 +0800

    added concat and debub dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var debug = require('gulp-debug');
+
+var js_path = 'photozzap/static/**/*.js';
 
 gulp.task('default', function() {
-  // place code for your default task here
+  gulp.src(js_path)
+  .pipe(debug());
+});
+
+
+
+gulp.task('scripts', function() {
+  gulp.src(js_path)
+  .pipe(uglify())
+  .pipe(gulp.dest('dist'));
 });
 
