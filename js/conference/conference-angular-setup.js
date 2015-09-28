194fec448aeed7c23f2eefc9370995a8bb5f9ce2
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-09-28 12:42:24 +0800

    added home route

--- js/conference/conference-angular-setup.js
+++ js/conference/conference-angular-setup.js
@@ -1,1 +1,11 @@
-var conferenceModule = angular.module('conferenceModule', ["firebase", 'ui.bootstrap', 'wu.masonry']);
+var conferenceModule = angular.module('conferenceModule', ['ui.router', "firebase", 'ui.bootstrap', 'wu.masonry']);
+
+conferenceModule.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home' , {
+            url: '/home',
+            templateUrl: 'partials/home.html'
+        });
+        
+    $urlRouterProvider.when('', '/home');
+}]);
