f22fbbc4a6a84e317559affe14383994b0566beb
lucw <luc.wastiaux@xsmail.com>
2016-05-02 05:34:38 +0000

    remove infinite loop by not watching bower_components

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var inject = require('gulp-inject');
 var webserver = require('gulp-webserver');
 var gulp_filter = require('gulp-filter');
 var watch = require('gulp-watch');
+var debug = require('gulp-debug');
 
 var bower_glob = './bower_components/**/*';
 var app_glob = './app/**/*';
@@ -13,24 +14,21 @@ var app_glob = './app/**/*';
 gulp.task('default', ['webserver_debug'], function() {
 });
 
-gulp.task('watch_copy_bower_components', function() {
-    gulp.src(bower_glob)
-    .pipe(watch(bower_glob))
-    .pipe(gulp.dest('debug/bower_components'));
-});
 
 gulp.task('watch_copy_debug', function() {
     gulp.src([app_glob])
     .pipe(watch(app_glob))
+    .pipe(debug({title: "updated"}))
     .pipe(gulp.dest('debug'));
 });
 
 
-gulp.task('webserver_debug', ['watch_copy_bower_components', 'watch_copy_debug'], function() {
+gulp.task('webserver_debug', ['watch_copy_debug'], function() {
     gulp.src('./debug/')
         .pipe(webserver({
-            livereload: {enable: false,
-                         port: 8081},
+            livereload: {enable: true,
+                         port: 8081
+            },
             directoryListing: {path: 'debug',
                                enable: true},
             open: true,
