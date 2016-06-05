fba4610f9f298042c1d7f828ea7f0730d062dcd2
lucw <luc.wastiaux@xsmail.com>
2016-06-05 05:23:26 +0000

    cleanup unused members

--- app/js/conference/conference-controller-photozzap.js
+++ app/js/conference/conference-controller-photozzap.js
@@ -2,29 +2,7 @@ conferenceModule.controller("PhotozzapCtrl",
 ["$scope", "$rootScope", "$log", "$window", "$filter", "$http", "$q", "$timeout", "$location", "$timeout", "$state", "$stateParams", "photozzapService", 
 function($scope, $rootScope, $log, $window, $filter, $http, $q, $timeout, $location, $timeout,  $state, $stateParams, photozzapService) {
    
-
-    $scope.show_default_nickname_notification = false;
-    $scope.sorted_notifications = [];
-
-    $scope.sorted_users = [];
-
-    
     $scope.logged_in_and_ready = false;
-    $scope.status_string = "loading";
-    
-    $scope.perform_setup_on_login = false;
-    $scope.new_nickname = undefined;
-   
-    $scope.watching_photo_index = false;
-    $scope.load_new_url_promise = undefined;
-    $scope.last_load_hires_timestamp = 0;
-    
-    $scope.watch_followed_user_handle = undefined;
-    $scope.followed_user_image_id = undefined;
-  
-    $scope.show_photo_counter = false;
-    $scope.show_photo_timeout = undefined;
-  
     $scope.conference_closed = false;
   
     $scope.init = function() {
