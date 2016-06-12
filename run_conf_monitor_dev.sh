75320968710acaef7601a03fb09ec445a06db566
lucw <luc.wastiaux@xsmail.com>
2016-06-12 07:26:51 +0000

    add start commandline

--- run_conf_monitor_dev.sh
+++ run_conf_monitor_dev.sh
@@ -1,2 +1,2 @@
 cd photozzap/nodejs
-PHOTOZZAP_CONFIG_FILE=photozzap2-dev.json PHOTOZZAP_ENV=dev PHOTOZZAP_FB_ROOT=photozzap2-dev.firebaseio.com node conference-monitor.js
+PHOTOZZAP_ENV=dev node conference-monitor.js
