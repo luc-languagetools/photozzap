23f670712108d78be59bea54a52191e81c0eda88
lucw <luc.wastiaux@xsmail.com>
2016-09-03 04:51:12 +0000

    upload site to www.photozzap.com as well

--- build_deploy_prod.sh
+++ build_deploy_prod.sh
@@ -1,2 +1,2 @@
 export ENVIRONMENT=prod
-rm -rf dist && rm -rf angular_config_generated && gulp build && surge -p ./dist/ -d p.zzap.co
+rm -rf dist && rm -rf angular_config_generated && gulp build && surge -p ./dist/ -d photo.zzap.co && surge -p ./dist/ -d www.photozzap.com
